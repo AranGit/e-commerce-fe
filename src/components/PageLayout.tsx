ac8c21f3265f1e3baef4ade66cff7e1e331ae9a7
Phop <aran.kochhiran@gmail.com>
2024-02-09 17:00:21 +0700

    Remove unused layout files and create PageLayout component

---
+++ src/components/PageLayout.tsx
@@ -0,0 +1,15 @@
+export default function PageLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <header></header>
+      <main className="flex min-h-screen flex-col items-center justify-between p-10 mt-[80px]">
+        {children}
+      </main>
+      <footer></footer>
+    </>
+  )
+}
